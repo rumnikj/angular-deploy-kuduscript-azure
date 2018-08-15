a1e9fb05b45461bf69f12fbfb6a5f1ea7c11791e
Thiago Passos <thiago@passos.com.au>
2018-08-16 09:11:44 +1000

    Upgrading to Angular 6

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,7 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 )
 
 :: 3. Angular Prod Build //If you had generated this yourself then please add this step manually!!)
-IF EXIST "%DEPLOYMENT_SOURCE%/.angular-cli.json" (
+IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
